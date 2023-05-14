bb422e0c59121cb936dd04c47a73d72824b06e67
Ronin2810 <adityapatilsy@gmail.com>
2023-05-14 11:59:48 +0530

    added validations

--- routes/player_router.js
+++ routes/player_router.js
@@ -3,9 +3,12 @@ const player_router = new express.Router()
 const axios = require("axios")
 
 player_router.post('/register',(req,res)=>{
-    const {name,email,password} = req.body;
+    let {name,email,password} = req.body;
+    email = email.trim();
+    password = password.trim();
+    name=name.trim();
     // send this data to backend and fetch it from axios and render/redirect accordingly
-    axios.post('https://pirates-of-web-backend-ronin2810.onrender.com/register',{name:name,email:email,password:password})
+    axios.post('http://localhost:5000/register',{name:name,email:email,password:password})
     .then((response)=>{
         if (response.data.res=="OK") {
             req.session.user_id =  response.data.cookie.user_id
@@ -26,9 +29,11 @@ player_router.post('/register',(req,res)=>{
     })
 })
 player_router.post('/player/login',(req,res)=>{
-    const {email,password} = req.body;
+    let {email,password} = req.body;
+    email = email.trim();
+    password = password.trim();
     // send this data to backend and fetch it from axios and render/redirect accordingly
-    axios.post('https://pirates-of-web-backend-ronin2810.onrender.com/player/login',{email:email,password:password})
+    axios.post('http://localhost:5000/player/login',{email:email,password:password})
     .then((response)=>{
         if (response.data.res=="OK") {
             req.session.user_id = response.data.cookie.user_id;
@@ -50,7 +55,7 @@ player_router.get('/player/dashboard',(req,res)=>{
     // fetch response from axios and render/redirect accordingly
     // console.log("isauth in /player/dashboard in frontend",req.session.isAuth)
     console.log(req.session);
-    axios.get('https://pirates-of-web-backend-ronin2810.onrender.com/player/dashboard',{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
+    axios.get('http://localhost:5000/player/dashboard',{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
     .then((response)=>{
         if (response.data.res=="OK") {
             res.render('player_dashboard',{name:response.data.name})
@@ -66,7 +71,7 @@ player_router.get('/player/dashboard',(req,res)=>{
 
 player_router.get('/player/riddle/restart',(req,res)=>{
     // fetch response from axios and render/redirect accordingly
-    axios.get('https://pirates-of-web-backend-ronin2810.onrender.com/player/riddle/restart',{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
+    axios.get('http://localhost:5000/player/riddle/restart',{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
     .then((response)=>{
         res.redirect('/player/riddle/q/1');
     })
@@ -77,10 +82,10 @@ player_router.get('/player/riddle/restart',(req,res)=>{
 
 player_router.get('/player/riddle/continue',(req,res)=>{
     // fetch response from axios and render/redirect accordingly
-    axios.get(`https://pirates-of-web-backend-ronin2810.onrender.com/player/riddle/continue`,{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
+    axios.get(`http://localhost:5000/player/riddle/continue`,{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
     .then((response)=>{
         if (response.data.res=="OK") {
-            res.redirect(`/player/riddle/q/${response.question}`)        
+            res.render(`q${response.data.question}`,{msg:""})        
         }
         else{
             res.redirect('/logout')
@@ -95,7 +100,7 @@ player_router.get('/player/riddle/continue',(req,res)=>{
 player_router.get('/player/riddle/q/:id',(req,res)=>{
     // fetch response from axios and render/redirect accordingly
     const q_id = req.params['id']
-    axios.get(`https://pirates-of-web-backend-ronin2810.onrender.com/player/riddle/q/${q_id}`,{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
+    axios.get(`http://localhost:5000/player/riddle/q/${q_id}`,{params:{isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity}})
     .then((response)=>{
         if (response.data.res=="OK") {
             res.render(`q${response.data.question}`,{msg:""});
@@ -117,8 +122,9 @@ player_router.get('/player/riddle/q/:id',(req,res)=>{
 player_router.post('/player/riddle/q/:id',(req,res)=>{
     const q_id = req.params['id']
     let {answer} = req.body;
+    answer= answer.trim()
     // send this data to backend and fetch it from axios and render/redirect accordingly
-    axios.post(`https://pirates-of-web-backend-ronin2810.onrender.com/player/riddle/q/${q_id}`,{answer:answer,isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity})
+    axios.post(`http://localhost:5000/player/riddle/q/${q_id}`,{answer:answer,isAuth:req.session.isAuth,user_id:req.session.user_id,email:req.session.email,entity:req.session.entity})
     .then((response)=>{
         if (response.data.res=="OK") {
             res.render(`q${response.data.question}`,{msg:response.data.msg});
@@ -144,7 +150,7 @@ player_router.post('/player/riddle/q/:id',(req,res)=>{
 player_router.get('/logout',(req,res)=>{
     console.log("inside logout from player router");
     const data = {isAuth:true}
-    axios.get('https://pirates-of-web-backend-ronin2810.onrender.com/logout',{params:data})
+    axios.get('http://localhost:5000/logout',{params:data})
     .then((response)=>{
         console.log(response.data);
         res.clearCookie(process.env.COOKIE_NAME)
@@ -158,7 +164,7 @@ player_router.get('/logout',(req,res)=>{
 
 player_router.get('/login',(req,res)=>{
     console.log("inside login from player router");
-    axios.get('https://pirates-of-web-backend-ronin2810.onrender.com/logout',{params:{isAuth:'true'}})
+    axios.get('http://localhost:5000/logout',{params:{isAuth:'true'}})
     .then((response)=>{
         console.log(response.data.msg);
         res.clearCookie(process.env.COOKIE_NAME)
